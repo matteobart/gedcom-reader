720901e383941a6962c5debaac28b9585b0e0917
DineenMichael <dineenmichael00@gmail.com>
2020-02-09 17:09:00 -0500

    merge

--- src/project_03_tables/main.py
+++ src/project_03_tables/main.py
@@ -11,14 +11,18 @@ people = []
 families = []
 parsed_output = []
 formatted_families = []
-individual_headers = ["id", "name", "gender", "birthday", "age", "alive", "death", "child", "spouse"]
-family_headers = ["id", "married", "divorced", "husbandId", "husbandName", "wifeId", "wifeName", "children"]
+individual_headers = ["id", "name", "gender", "birthday",
+                      "age", "alive", "death", "child", "spouse"]
+family_headers = ["id", "married", "divorced", "husbandId",
+                  "husbandName", "wifeId", "wifeName", "children"]
 
 individual_table = PrettyTable()
 family_table = PrettyTable()
 
-#given an array of strings parse the GEDCOM information
-#there is no return but will print to console
+# given an array of strings parse the GEDCOM information
+# there is no return but will print to console
+
+
 def create_individual_list(parsed_output):
     start_case = False
     for line in parsed_output:
@@ -39,11 +43,11 @@ def create_individual_list(parsed_output):
             new_person_map['gender'] = line[1]
         elif line[0] == 'DATE':
             date = datetime.strptime(line[1], '%d %b %Y')
-            diff = relativedelta(datetime.now(), date) 
+            diff = relativedelta(datetime.now(), date)
             date = str(date)
             birthday = date.split(' ')
             new_person_map['birthday'] = birthday[0]
-            new_person_map['age'] = diff.years 
+            new_person_map['age'] = diff.years
         elif line[0] == 'FAMS':
             line[1] = line[1].replace('@', '')
             if 'spouse' in new_person_map.keys():
@@ -91,7 +95,7 @@ def create_family_list(parsed_output):
         elif line[0] == "HUSB":
             line[1] = line[1].replace('@', '')
             z.husbandId = line[1]
-            z.husbandName= person_lookup(line[1])
+            z.husbandName = person_lookup(line[1])
         elif line[0] == "WIFE":
             line[1] = line[1].replace('@', '')
             z.wifeId = line[1]
@@ -100,27 +104,30 @@ def create_family_list(parsed_output):
             line[1] = line[1].replace('@', '')
             if len(z.children) > 0:
                 z.children.append(line[1])
-            else: 
+            else:
                 z.children = [line[1]]
     families.append(z)
-            
+
+
 def person_lookup(ID):
     for person in people:
         if person['id'] == ID:
             return person['name']
 
+
 def build_table(table, headers, data):
     for header in headers:
         column = [item[header] for item in data]
         table.add_column(header, column)
-    print(table)
+    print(table.get_string(sortby="id"))
+
 
 def parse(lines):
     for line in lines:
         line = line.strip()
         line = line.replace("\n", "")
         split = line.split(" ", 2)
-        if len(split) == 2: #if it has no extra args, add an empty one
+        if len(split) == 2:  # if it has no extra args, add an empty one
             split.append("")
         if split[0] == "0":
             if split[2] == "INDI":
@@ -137,18 +144,18 @@ def parse(lines):
                 print_out(0, split[1], False, split[2])
         elif split[0] == "1":
             possible_tags = [
-                "NAME", 
-                "SEX", 
-                "BIRT", 
-                "DEAT", 
-                "FAMC", 
-                "FAMS", 
-                "MARR", 
-                "HUSB", 
+                "NAME",
+                "SEX",
+                "BIRT",
+                "DEAT",
+                "FAMC",
+                "FAMS",
+                "MARR",
+                "HUSB",
                 "WIFE",
                 "CHIL",
                 "DIV"
-                ]
+            ]
             if split[1] in possible_tags:
                 print_out(1, split[1], True, split[2])
             else:
@@ -158,24 +165,28 @@ def parse(lines):
                 print_out(2, "DATE", True, split[2])
             else:
                 print_out(2, split[1], False, split[2])
-        else: 
+        else:
             print_out(split[0], split[1], False, split[2])
+
 
 def print_in(str):
     print("-->", str)
 
+
 def print_out(level, tag, valid, arg):
-    if valid: 
+    if valid:
         # valid_str = "<--" + str(level) + "|" + str(tag) + "|" + ("Y" if valid else "N") + "|" + str(arg)
         valid_str_arr = [str(tag), str(arg)]
         parsed_output.append(valid_str_arr)
         # print(parsed_output[-1])
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Homemade GEDCOM reader')
-    parser.add_argument('GEDCOM_file', type=str, help='The file to read (must be in GEDCOM format')
+    parser.add_argument('GEDCOM_file', type=str,
+                        help='The file to read (must be in GEDCOM format')
     args = parser.parse_args()
-    
+
     with open(args.GEDCOM_file) as file:
         lines = file.readlines()
         parse(lines)
@@ -188,16 +199,3 @@ if __name__ == "__main__":
     build_table(individual_table, individual_headers, people)
     print("--------------------------------------FAMILY TABLE--------------------------------------")
     build_table(family_table, family_headers, formatted_families)
-    
-    
-
-
-
-
-
-
-
-
-
-
-
